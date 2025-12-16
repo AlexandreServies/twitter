9c6712d58502525e8580b0ce151aaa2fedb5894a
Alexandre Servies <alex.servies@gmail.com>
2025-12-16 04:47:27 -0800

    Add request/response logging for all endpoints

--- src/main/java/com/bark/twitter/controller/TwitterController.java
+++ src/main/java/com/bark/twitter/controller/TwitterController.java
@@ -2,6 +2,8 @@ package com.bark.twitter.controller;
 
 import com.bark.twitter.service.TwitterService;
 import com.fasterxml.jackson.databind.JsonNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +15,8 @@ import java.util.Map;
 @RestController
 public class TwitterController {
 
+    private static final Logger log = LoggerFactory.getLogger(TwitterController.class);
+
     private final TwitterService twitterService;
 
     public TwitterController(TwitterService twitterService) {
@@ -21,18 +25,29 @@ public class TwitterController {
 
     @GetMapping("/tweet/{id}")
     public JsonNode getTweet(@PathVariable String id) {
-        return twitterService.getTweet(id);
+        long start = System.currentTimeMillis();
+        log.info("[{}][TWEET][{}] GET /tweet/{}", start, id, id);
+        JsonNode response = twitterService.getTweet(id);
+        log.info("[{}][TWEET][{}] {}", System.currentTimeMillis(), id, response);
+        return response;
     }
 
     @GetMapping("/user/{id}")
     public JsonNode getUser(@PathVariable String id) {
-        return twitterService.getUser(id);
+        long start = System.currentTimeMillis();
+        log.info("[{}][USER][{}] GET /user/{}", start, id, id);
+        JsonNode response = twitterService.getUser(id);
+        log.info("[{}][USER][{}] {}", System.currentTimeMillis(), id, response);
+        return response;
     }
 
     @GetMapping("/community/{id}")
     public ResponseEntity<Map<String, String>> getCommunity(@PathVariable String id) {
-        return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED)
-                .body(Map.of("error", "Community lookup not implemented"));
+        long start = System.currentTimeMillis();
+        log.info("[{}][COMMUNITY][{}] GET /community/{}", start, id, id);
+        Map<String, String> response = Map.of("error", "Community lookup not implemented");
+        log.info("[{}][COMMUNITY][{}] {}", System.currentTimeMillis(), id, response);
+        return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).body(response);
     }
 
     @GetMapping("/health")
