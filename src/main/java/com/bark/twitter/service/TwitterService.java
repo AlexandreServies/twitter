8225bffd9b16c6d04dbd7875d87fa73c3bd85cb1
Alexandre Servies <alex.servies@gmail.com>
2025-12-16 04:42:46 -0800

    Enrich reply tweets with full data from replied-to tweet

--- src/main/java/com/bark/twitter/service/TwitterService.java
+++ src/main/java/com/bark/twitter/service/TwitterService.java
@@ -3,6 +3,7 @@ package com.bark.twitter.service;
 import com.bark.twitter.client.SynopticClient;
 import com.bark.twitter.exception.NotFoundException;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
@@ -17,8 +18,31 @@ public class TwitterService {
 
     @Cacheable(value = "tweets", key = "#tweetId")
     public JsonNode getTweet(String tweetId) {
-        return synopticClient.getTweet(tweetId)
+        JsonNode tweet = synopticClient.getTweet(tweetId)
                 .orElseThrow(() -> new NotFoundException("Tweet not found: " + tweetId));
+
+        return enrichReplyData(tweet);
+    }
+
+    private JsonNode enrichReplyData(JsonNode tweet) {
+        JsonNode replyNode = tweet.get("reply");
+        if (replyNode == null || replyNode.isNull()) {
+            return tweet;
+        }
+
+        JsonNode replyToStatusId = replyNode.get("reply_to_status_id");
+        if (replyToStatusId == null || replyToStatusId.isNull()) {
+            return tweet;
+        }
+
+        String repliedToTweetId = replyToStatusId.asText();
+        JsonNode repliedToTweet = synopticClient.getTweet(repliedToTweetId).orElse(null);
+
+        if (repliedToTweet != null) {
+            ((ObjectNode) tweet).set("reply", repliedToTweet);
+        }
+
+        return tweet;
     }
 
     @Cacheable(value = "users", key = "#userId")
