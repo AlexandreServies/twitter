41f15f71c974a946ad29dcbd0dc78e8b61683486
Alexandre Servies <alex.servies@gmail.com>
2025-12-16 04:58:32 -0800

    Add cache hit logging with programmatic caching

--- src/main/java/com/bark/twitter/service/TwitterService.java
+++ src/main/java/com/bark/twitter/service/TwitterService.java
@@ -4,24 +4,38 @@ import com.bark.twitter.client.SynopticClient;
 import com.bark.twitter.exception.NotFoundException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TwitterService {
 
     private final SynopticClient synopticClient;
+    private final Cache tweetsCache;
+    private final Cache usersCache;
+    private final Cache communitiesCache;
 
-    public TwitterService(SynopticClient synopticClient) {
+    public TwitterService(SynopticClient synopticClient, CacheManager cacheManager) {
         this.synopticClient = synopticClient;
+        this.tweetsCache = cacheManager.getCache("tweets");
+        this.usersCache = cacheManager.getCache("users");
+        this.communitiesCache = cacheManager.getCache("communities");
     }
 
-    @Cacheable(value = "tweets", key = "#tweetId")
     public JsonNode getTweet(String tweetId) {
+        JsonNode cached = tweetsCache.get(tweetId, JsonNode.class);
+        if (cached != null) {
+            System.out.println("[" + System.currentTimeMillis() + "][TWEET][" + tweetId + "] Cache hit");
+            return cached;
+        }
+
         JsonNode tweet = synopticClient.getTweet(tweetId)
                 .orElseThrow(() -> new NotFoundException("Tweet not found: " + tweetId));
 
-        return enrichReplyData(tweet);
+        JsonNode enriched = enrichReplyData(tweet);
+        tweetsCache.put(tweetId, enriched);
+        return enriched;
     }
 
     private JsonNode enrichReplyData(JsonNode tweet) {
@@ -45,14 +59,27 @@ public class TwitterService {
         return tweet;
     }
 
-    @Cacheable(value = "users", key = "#userId")
     public JsonNode getUser(String userId) {
-        return synopticClient.getUser(userId)
+        JsonNode cached = usersCache.get(userId, JsonNode.class);
+        if (cached != null) {
+            System.out.println("[" + System.currentTimeMillis() + "][USER][" + userId + "] Cache hit");
+            return cached;
+        }
+
+        JsonNode user = synopticClient.getUser(userId)
                 .orElseThrow(() -> new NotFoundException("User not found: " + userId));
+
+        usersCache.put(userId, user);
+        return user;
     }
 
-    @Cacheable(value = "communities", key = "#communityId")
     public JsonNode getCommunity(String communityId) {
+        JsonNode cached = communitiesCache.get(communityId, JsonNode.class);
+        if (cached != null) {
+            System.out.println("[" + System.currentTimeMillis() + "][COMMUNITY][" + communityId + "] Cache hit");
+            return cached;
+        }
+
         // Not implemented yet
         throw new UnsupportedOperationException("Community lookup not implemented");
     }
